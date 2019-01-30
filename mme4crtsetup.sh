0c63a95c945bfeb604d1bec6efb63d804c516af6
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-30 23:04:32 +0000

    Update mme4crtsetup.sh

--- mme4crtsetup.sh
+++ mme4crtsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NC = "\033[0m"
+NC="\033[0m"
 version=$(lsb_release -c -s)
 
 
@@ -30,7 +30,7 @@ echo "$STR"
 
 echo -e "\033[32mCreating .xprofile using output "$output "\033[0m"
 echo "$STR"
-exec 3<> .xprofile2
+exec 3<> .xprofile
 	echo "#!/bin/sh" >&3
 	echo "xrandr --newmode \"700x480_59.941002\" 13.849698 700 742 801 867 480 490 496 533 interlace -hsync -vsync" >&3
 	echo "xrandr --addmode "$output" 700x480_59.941002" >&3
