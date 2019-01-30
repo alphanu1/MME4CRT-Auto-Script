486a0208866311a73f32091d0cb97fbef800e2ed
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-30 20:39:24 +0000

    Update mme4crtsetup.sh

--- mme4crtsetup.sh
+++ mme4crtsetup.sh
@@ -93,7 +93,7 @@ echo "$STR"
 sleep 1
 
 sudo apt-get build-dep retroarch -y
-sudo apt-get install git build-essential libgl-dev libudev-dev libasound2-dev libpulse-dev libsdl2-dev libvulkan-dev libjack-dev -y
+sudo apt-get install git build-essential libasound2-dev libpulse-dev libsdl2-dev libvulkan-dev libjack-dev -y
 echo "$STR"
 echo -e "\033[32mDone\033[0m"
 echo "$STR"
